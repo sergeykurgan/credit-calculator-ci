16a63444de919985337ea1a62ba18837070feb69
sergeykurgan <dezmond208@gmail.com>
2025-12-08 02:14:28 +0700

    Update test_api.py

--- tests/test_api.py
+++ tests/test_api.py
@@ -5,7 +5,7 @@ import sys
 import pytest
 import requests
 
-# добавляем корень проекта в sys.path
+# добавляем корень проекта в sys.path, чтобы работал import api
 PROJECT_ROOT = os.path.dirname(os.path.dirname(__file__))
 if PROJECT_ROOT not in sys.path:
     sys.path.insert(0, PROJECT_ROOT)
